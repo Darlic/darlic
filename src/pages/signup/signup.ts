d521bf94d467d29372c9693fcab0d85cc0eaa945
Radha <git@genx-infotech.com>
2018-05-26 16:02:04 +0530

    validation on signin page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {LoginPage} from '../../pages/login/login';
+import { FormArray, FormBuilder, FormGroup,Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,14 +9,29 @@ import {LoginPage} from '../../pages/login/login';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  signup:FormGroup;
+  constructor(public fb:FormBuilder,public navCtrl: NavController, public navParams: NavParams) {
+    this.createSignup();
+  }
+  createSignup() {
+    this.signup=this.fb.group({
+      firstName:["",Validators.required],
+      lastName:["",Validators.required],
+      Email:["",Validators.required],
+      password:["",Validators.required],
+     
+    })
+  }
+  login(signup){
+    if(!(signup.valid)){
+      console.log('not valid');
+      this.signup;
+    }else{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-  ionViewDidLoad() {
-   
-  }
-  login(){
+    console.log(signup.value);
   	this.navCtrl.setRoot(LoginPage);
+    }
+
   }
 
 }
